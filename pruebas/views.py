c6740a9b93ea7d3dcb9d214b8dd5b225688bae60
Martino Pizzol <martino.pizzol@gmail.com>
2015-01-31 16:00:07 +0100

    Added threshold for testing document group

--- pruebas/views.py
+++ pruebas/views.py
@@ -41,8 +41,9 @@ def model_document_group(request, dg_pk, datatxt_id):
         dg = DocumentGroup.objects.get(pk=dg_pk)
     except DocumentGroup.DoesNotExist:
         raise Http404
+    threshold = request.GET.get('threshold', 0.25)
     #launch a celery task with this model
-    task = test_document_set.delay(model, dg)
+    task = test_document_set.delay(model, dg, threshold)
     dg.testing_task_id = task
     dg.save()
     return HttpResponse(
