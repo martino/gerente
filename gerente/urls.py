f8d076bf559f6d4fc0e7529875b6b0aaf5b9af16
Martino Pizzol <martino.pizzol@gmail.com>
2015-02-07 20:38:05 +0100

    Prefixed all urls

--- gerente/urls.py
+++ gerente/urls.py
@@ -2,12 +2,16 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-urlpatterns = patterns(
+api_patterns = patterns(
     '',
-    # Examples:
     url(r'', include('clasificador.urls')),
     url(r'', include('pruebas.urls')),
     url(r'', include('documentos.urls')),
     url(r'^tasks/', include('djcelery.urls')),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns = patterns(
+    '',
+    url(r'^api/', include(api_patterns)),
 )
