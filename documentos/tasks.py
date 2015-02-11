e0bbee329e1afe467f9104efa360db3539926e94
Martino Pizzol <martino.pizzol@gmail.com>
2015-02-12 00:38:30 +0100

    Better handling dropbox download links, recursion on extracted zip file and better handing of encodings

--- documentos/tasks.py
+++ documentos/tasks.py
@@ -3,16 +3,23 @@ import os
 import zipfile
 import requests
 import tempfile
+import fnmatch
 from celery import shared_task
 from codecs import open
 from documentos.models import BaseDocument
 
 
 def import_document(dg, file_path):
+    print "Import doucment {}".format(file_path)
     file_name = file_path.split('/')[-1]
 
     with open(file_path) as f:
         text = f.read()
+
+    try:
+        text = text.encode("utf-8")
+    except:
+        text = text.decode('latin1').encode("utf-8")
 
     doc = BaseDocument.objects.create(
         file_name=file_name,
@@ -28,6 +35,9 @@ def import_documents(dg, url):
     filename = None
     ret_code = 0
     try:
+        # small fixes for url download
+        url = url.replace('www.dropbox', 'dl.dropbox')
+
         # download zip file
         remote_filename = url.split('/')[-1]
         suffix = '.' + remote_filename.split('.')[-1]
@@ -40,16 +50,21 @@ def import_documents(dg, url):
             filename = f_tmp.name
 
         if zipfile.is_zipfile(filename):
+            print "{} is a zip".format(filename)
             # if is a zip file, extract it into a temp directory
             zf = zipfile.ZipFile(filename)
             tmp_dir = tempfile.mkdtemp()
             zf.extractall(tmp_dir)
-            files = glob.glob(os.path.join(tmp_dir, '*'))
+            files = []
+            for root, dirnames, filenames in os.walk(tmp_dir):
+                for fnametmp in fnmatch.filter(filenames, '*'):
+                    files.append(os.path.join(root, fnametmp))
+
             # analyze all extracted file
             # TODO handle exception if a user send binary file
-            for filename in files:
-                if not os.path.isdir(filename):
-                    all_docs_pk.append(import_document(dg, filename))
+            for fname in files:
+                if not os.path.isdir(fname):
+                    all_docs_pk.append(import_document(dg, fname))
         else:
             all_docs_pk.append(import_document(dg, filename))
 
@@ -59,9 +74,9 @@ def import_documents(dg, url):
         BaseDocument.objects.filter(pk__in=all_docs_pk).delete()
         ret_code = 1
     finally:
-        if filename is not None:
-            os.remove(filename)
         dg.importing_task_id = None
         dg.save()
+        if filename is not None:
+            os.remove(filename)
 
     return ret_code
