b8a0812c561e57dcd568baf98f52ff040043dec2
Martino Pizzol <martino.pizzol@gmail.com>
2015-03-10 07:40:49 +0100

    Fixed phrases splitting rules

--- documentos/helpers.py
+++ documentos/helpers.py
@@ -16,7 +16,7 @@ def split_document(text, use_paragraph=True):
     parts = text.split('.\n')
     if len(parts) == 1:
         parts = text.split('.\r\n')
-    if len(parts) == 1:
+    if len(parts) < 3:
         parts = text.split('.')
     return parts
 
